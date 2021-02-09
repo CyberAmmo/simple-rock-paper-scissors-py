0c5c22db8beaa1c6e75d2c1bc21d159497bd5f14
Vlad <superman.back@ukr.net>
2021-02-09 22:01:21 +0200

    Add a new game

---
+++ rps_game.py
@@ -0,0 +1,19 @@
+import random
+
+def game():
+    user = input("Enter a 'r' for rock, 'p' for paper and 's' for scissors\n")
+    comp = random.choice(['r', 'p', 's'])
+    print(comp)
+    if user == comp:
+        return 'None survive'
+
+    if is_win(user, comp):
+        return 'You win!!'
+    return 'You Lost'
+
+def is_win(player, opponent):
+    # r > s, p > r, s > p
+    if (player == 'r' and opponent == 's') or (player == 'p' and opponent == 'r') or (player == 's' and opponent == 'p'):
+        return True
+
+print(game())
